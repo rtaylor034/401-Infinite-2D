36940d1a37440e2c33df802e3dd0a4f428db7517
GFunction <ryan.mm.taylor@gmail.com>
2022-11-10 15:36:59 -0800

    Added UniversalRotation

--- Assets/Framework/Player.cs
+++ Assets/Framework/Player.cs
@@ -21,6 +21,24 @@ public class Player
         Red
     }
 
+    /// <summary>
+    /// Gets the universal rotation of a team. <br></br>
+    /// > Meant to be used with <see cref="BoardCoords.Rotate(IEnumerable{Vector3Int}, Vector3Int, int)"/>.
+    /// </summary>
+    /// <param name="team"></param>
+    /// <remarks>
+    /// - Ex: Red returns 3, because all relative coordinates from Red's perspective should be mirrored (rotated 3 times).
+    /// </remarks>
+    public static sbyte GetUniversalRotation(this ETeam team)
+    {
+        return team switch
+        {
+            ETeam.Blue => 0,
+            ETeam.Red => 3,
+            _ => 0,
+        };
+    }
+
     public static Player DummyPlayer => new Player(ETeam.NONE);
 
     //Player class stores a player's Abilities, Passive, Control spheres, etc.
@@ -28,6 +46,6 @@ public class Player
     //Players are *not* Units. Units are the peices that move across the board that have HP, status effects, etc. , while Player objects hold information about the entire side, such as the cards, passive, etc. a player has.
     public override string ToString()
     {
-        return $"*{Team}*";
+        return $"P*{Team}";
     }
 }

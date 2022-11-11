104e643dfa3020e48cbeb8726f8b5c0ddaf7f21e
GlobbyLS <114969407+rtaylor034@users.noreply.github.com>
2022-11-11 08:39:22 -0800

    unf doc turn

--- Assets/Framework/GameActions/[Turn].cs
+++ Assets/Framework/GameActions/[Turn].cs
@@ -7,9 +7,18 @@ public abstract partial class GameAction
 
     public class Turn : GameAction
     {
+        /// <summary>
+        /// Triggered when any <see cref="Turn"/> is performed.
+        /// </summary>
         public static event GameActionEventHandler<Turn> OnPerform;
 
+        /// <summary>
+        /// The <see cref="Player"/> that ended their turn.
+        /// </summary>
         public Player FromPlayer { get; private set; }
+        /// <summary>
+        /// The <see cref="Player"/> that started their turn.
+        /// </summary>
         public Player ToPlayer { get; private set; }
 
         protected override void InternalPerform()
@@ -29,6 +38,11 @@ public abstract partial class GameAction
             ToPlayer = toPlayer;
         }
 
+        /// <summary>
+        /// Declare a <see cref="Turn"/>, transferring turn control from <paramref name="fromPlayer"/> to <paramref name="toPlayer"/>.
+        /// </summary>
+        /// <param name="fromPlayer"></param>
+        /// <param name="toPlayer"></param>
         public static void Declare(Player fromPlayer, Player toPlayer)
         {
             FinalizeDeclare(new Turn(fromPlayer, toPlayer));

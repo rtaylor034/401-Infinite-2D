a86d3e4f9f7ca2509b71035f1bad71dcfa0ddd05
GFunction <ryan.mm.taylor@gmail.com>
2022-11-12 13:19:06 -0800

    Documented EnergyChange

--- Assets/Framework/GameActions/[EnergyChange].cs
+++ Assets/Framework/GameActions/[EnergyChange].cs
@@ -10,11 +10,24 @@ public abstract partial class GameAction
     /// </summary>
     public class EnergyChange : GameAction
     {
-
+        /// <summary>
+        /// Occurs when any <see cref="EnergyChange"/> is performed.
+        /// </summary>
         public static event GameActionEventHandler<EnergyChange> OnPerform;
 
+        /// <summary>
+        /// The <see cref="Player"/> that recieved the change in energy on this action.
+        /// </summary>
         public Player Reciever { get; private set; }
+        /// <summary>
+        /// The amount of energy that Reciever had before this action. <br></br>
+        /// <i>Reciever's energy is set to this amount when this action is undone.</i>
+        /// </summary>
         public int BeforeAmount { get; private set; }
+        /// <summary>
+        /// The amount of energy that Reciever had after this action. <br></br>
+        /// <i>Reciever's energy is set to this amount when this action is performed.</i>
+        /// </summary>
         public int AfterAmount { get; private set; }
 
         protected override void InternalPerform()
@@ -35,6 +48,17 @@ public abstract partial class GameAction
             AfterAmount = after;
         }
 
+        /// <summary>
+        /// Change <paramref name="reciever"/>'s energy amount by the <paramref name="changeFunction"/>. <br></br>
+        /// > This <see cref="EnergyChange"/> will be a resultant of <paramref name="resultOf"/>.
+        /// </summary>
+        /// <remarks>
+        /// <i>Ex: <c><paramref name="changeFunction"/> = (e) => { return e + 1; }</c> <br></br>
+        /// This would add 1 to <paramref name="reciever"/>'s energy amount.</i>
+        /// </remarks>
+        /// <param name="resultOf"></param>
+        /// <param name="reciever"></param>
+        /// <param name="changeFunction"></param>
         public static void DeclareAsResultant(GameAction resultOf, Player reciever, Func<int, int> changeFunction)
         {
             resultOf.AddResultant(new EnergyChange(resultOf.Performer, reciever, reciever.Energy, changeFunction(reciever.Energy)));

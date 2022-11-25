84e3739d590ebcaf6e0f8e89ad7d63b7927f32d2
GFunction <ryan.mm.taylor@gmail.com>
2022-11-25 00:20:47 -0800

    Update [PlayAbility].cs

--- Assets/Framework/GameActions/[PlayAbility].cs
+++ Assets/Framework/GameActions/[PlayAbility].cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public partial class GameAction
 {
 
+    //READPLEASE: OnPerform needs be changed on OnConstruct (triggers when GameAction is created) if GameActions are to be fully undo/redo safe. As it stands, if a GameAction is performed multiple times, duplicate GameActions will be declared as resultants every time the parent GameAction is performed past the first time.
     public class PlayAbility : GameAction
     {
         public Ability PlayedAbility { get; private set; }

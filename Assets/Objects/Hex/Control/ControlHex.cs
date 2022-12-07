cbc0bca2fe7ed1c44cf9e6c7af1ed4778705dfcc
GFunction <ryan.mm.taylor@gmail.com>
2022-12-07 09:30:37 -0800

    added IsLocked to ControlHex

--- Assets/Objects/Hex/Control/ControlHex.cs
+++ Assets/Objects/Hex/Control/ControlHex.cs
@@ -5,6 +5,15 @@ using UnityEngine;
 public class ControlHex : Hex
 {
 
-    //No unique behavior implemented
+    public bool IsLocked { get
+        {
+            HashSet<Player.ETeam> teams = new();
+            foreach (Hex hex in _board.HexDict.Values)
+            {
+                if (hex is not ControlHex chex) continue;
+                if (chex.Occupant is not null && !teams.Add(chex.Occupant.Team)) return true;
+            }
+            return false;
+        } }
 
 }

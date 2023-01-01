afd5bd1b42c08ee37272b5bc2bed14d931cf4cfc
GFunction <ryan.mm.taylor@gmail.com>
2022-12-31 16:06:00 -0800

    Info to MoveInfo

--- Assets/Framework/GameActions/[Move].cs
+++ Assets/Framework/GameActions/[Move].cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.Scripting.APIUpdating;
 
 public partial class GameAction
 {
@@ -13,7 +15,7 @@ public partial class GameAction
     /// </summary>
     public abstract class Move : GameAction
     {
-        public Info MoveInfo { get; private set; }
+        public MoveInfo Info { get; private set; }
         public List<PositionChange> PositionChanges => new(_positionChanges);
         private readonly List<PositionChange> _positionChanges;
 
@@ -25,14 +27,19 @@ public partial class GameAction
                 await AddResultant(change);
         }
 
-        public Move(Player performer, Info moveInfo, IEnumerable<PositionChange> positionChanges) : base(performer)
+        public Move(Player performer, MoveInfo moveInfo, IEnumerable<PositionChange> positionChanges) : base(performer)
         {
             _positionChanges = new(positionChanges);
-            MoveInfo = moveInfo;
+            Info = moveInfo;
 
         }
 
-        public abstract record Info
+        public static async Task<Move> Prompt(Player performer, MoveInfo moveInfo, Action<Selector.SelectionArgs> cancelCallback)
+        {
+            throw new NotImplementedException();
+        }
+
+        public abstract record MoveInfo
         {   
             public HashSet<Unit> MovingUnits { get; private set; }
             public Func<Unit, Predicate<Hex>> FinalCondition { get; private set; } =
@@ -43,46 +50,48 @@ public partial class GameAction
             hex.IsOccupiable;
             public static readonly Func<Unit, Predicate<Hex>> GUARDED_BASE_CHECK = unit => hex =>
             !(hex is BaseHex bhex && bhex.IsGuarded && bhex.Team != unit.Team);
-            public Info(IEnumerable<Unit> movingUnits)
-            {
-                MovingUnits = new(movingUnits);
-            }
-            public Info(params Unit[] movingUnits)
+            protected MoveInfo(IEnumerable<Unit> movingUnits)
             {
                 MovingUnits = new(movingUnits);
             }
 
-            public record Positional : Info
+        }
+
+        public record PositionalInfo : MoveInfo
+        {
+            public Vector3Int Anchor { get; private set; }
+            public HashSet<Vector3Int> PositionOffsets { get; private set; }
+
+            public static HashSet<Vector3Int> IN_FRONT => new() { BoardCoords.up };
+            public static HashSet<Vector3Int> BEHIND => new() { -BoardCoords.up };
+            public static HashSet<Vector3Int> ADJACENT => new(Vector3Int.zero.GetAdjacent());
+
+            public PositionalInfo(IEnumerable<Unit> movingUnits) : base(movingUnits) { }
+            public PositionalInfo(params Unit[] movingUnits) : base(movingUnits) { }
+        }
+
+        public record PathedInfo : MoveInfo
+        {
+            public int Distance { get; private set; }
+            public int MinDistance { get; private set; } = 0;
+            public Func<Unit, Board.ContinuePathCondition> PathingCondition { get; private set; } = STD_COLLISION;
+            public Func<Unit, Board.ContinuePathCondition> PathingOverride { get; private set; } = _ => (_, _) => false;
+            public Func<Unit, IEnumerable<(Vector3Int Anchor, ERadiusRule Rule)>> DirectionalBlocks { get; private set; } = _ => new (Vector3Int, ERadiusRule)[0];
+            //add all individiual weight functions together to get final weight
+            public Func<Unit, Board.PathWeightFunction> PathingWeightFunction { get; private set; } = _ => (_, _) => 1;
+
+            public static readonly Func<Unit, Board.ContinuePathCondition> STD_COLLISION = unit => (_, hex) =>
+            !(hex.BlocksPathing || (hex.Occupant != null && hex.Occupant.Team != unit.Team));
+
+            public enum ERadiusRule : sbyte
             {
-                public Vector3Int Anchor { get; private set; }
-                public HashSet<Vector3Int> PositionOffsets { get; private set; }
-
-                public static HashSet<Vector3Int> IN_FRONT => new() { BoardCoords.up };
-                public static HashSet<Vector3Int> BEHIND => new() { -BoardCoords.up };
-                public static HashSet<Vector3Int> ADJACENT => new(Vector3Int.zero.GetAdjacent());
-
+                Toward = -1,
+                Around = 0,
+                Away = 1
             }
 
-            public record Pathed : Info
-            {
-                public int Distance { get; private set; }
-                public int MinDistance { get; private set; } = 0;
-                public Func<Unit, Board.ContinuePathCondition> PathingCondition { get; private set; } = STD_COLLISION;
-                public Func<Unit, Board.ContinuePathCondition> PathingOverride { get; private set; } = _ => (_, _) => false;
-                public Func<Unit, IEnumerable<(Vector3Int Anchor, ERadiusRule Rule)>> DirectionalBlocks { get; private set; } = _ => new (Vector3Int, ERadiusRule)[0];
-                //add all individiual weight functions together to get final weight
-                public Func<Unit, Board.PathWeightFunction> PathingWeightFunction { get; private set; } = _ => (_, _) => 1;
-
-                public static readonly Func<Unit, Board.ContinuePathCondition> STD_COLLISION = unit => (_, hex) =>
-                !(hex.BlocksPathing || (hex.Occupant != null && hex.Occupant.Team != unit.Team));
-
-                public enum ERadiusRule : sbyte
-                {
-                    Toward = -1,
-                    Around = 0,
-                    Away = 1
-                }
-            }
+            public PathedInfo(IEnumerable<Unit> movingUnits) : base(movingUnits) { }
+            public PathedInfo(params Unit[] movingUnits) : base(movingUnits) { }
         }
     }
 }

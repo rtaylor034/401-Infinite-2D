268edd6d19cc9188402c81bf97adeaca4abb7293
GFunction <ryan.mm.taylor@gmail.com>
2023-01-06 14:33:13 -0800

    doc PositionChange

--- Assets/Framework/GameActions/[PositionChange].cs
+++ Assets/Framework/GameActions/[PositionChange].cs
@@ -7,7 +7,6 @@ using System.Threading.Tasks;
 using Unity.Mathematics;
 using Unity.VisualScripting;
 using UnityEngine;
-using static System.Collections.Specialized.BitVector32;
 
 public abstract partial class GameAction
 {
@@ -16,13 +15,31 @@ public abstract partial class GameAction
     /// </summary>
     public class PositionChange : GameAction
     {
+        /// <summary>
+        /// The <see cref="Unit"/> that had its position changed.
+        /// </summary>
         public Unit AffectedUnit { get; private set; }
+        
+        //consider using an offset that gets added to the position instesad of FromPos and ToPos to stay consistent with the rest of the "Change" GameActions.
+        /// <summary>
+        /// The position that AffectedUnit was at before it was changed.
+        /// </summary>
         public Vector3Int FromPos { get; private set; }
+        /// <summary>
+        /// The position that AffectedUnit's position was changed to.
+        /// </summary>
         public Vector3Int ToPos { get; private set; }
 
         protected override void InternalPerform() => AffectedUnit.UpdatePosition(ToPos);
         protected override void InternalUndo() => AffectedUnit.UpdatePosition(FromPos);
 
+        /// <summary>
+        /// Creates a <see cref="PositionChange"/> action, changing <paramref name="unit"/>'s position from <paramref name="fromPos"/> to <paramref name="toPos"/>, by <paramref name="performer"/>.
+        /// </summary>
+        /// <param name="performer"></param>
+        /// <param name="unit"></param>
+        /// <param name="fromPos"></param>
+        /// <param name="toPos"></param>
         public PositionChange(Player performer, Unit unit, Vector3Int fromPos, Vector3Int toPos) : base(performer)
         {
             AffectedUnit = unit;

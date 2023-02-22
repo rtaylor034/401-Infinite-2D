8271195477c8a8c3248a027ee75fc9574965c454
GFunction <ryan.mm.taylor@gmail.com>
2023-02-22 15:53:42 -0800

    cleaned it up

--- Assets/Framework/Registries/AbilityRegistry.cs
+++ Assets/Framework/Registries/AbilityRegistry.cs
@@ -4,7 +4,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using UnityEngine;
-using static Ability.Unsourced;
+using static Ability;
 
 public static class AbilityRegistry
 {
@@ -19,66 +19,55 @@ public static class AbilityRegistry
 
         List<ConstructionTemplate<Ability>> masterList = new()
         {
-            //>0 LANCE
-            () => new Ability.Sourced
-            (
-
-                "Lance",
-                Ability.ETypeIdentity.Attack,
-                new ConstructionTemplate<UnitEffect>[]
+            () => new Sourced("Lance", ETypeIdentity.Attack)
+            {
+                TargetEffects = new()
                 {
                     () => new UnitEffect.Slow(STD_DURATION),
                     () => new UnitEffect.Damage(STD_DURATION)
                 },
-                new HashSet<Vector3Int>
+                HitArea = new()
                 {
                     Hit(0, 1, 0),
                     Hit(0, 2, 0),
                     Hit(0, 3, 0)
                 },
-                new Ability.PlayAction(async a =>
+                TargetingConditions = new()
+                {
+                    Sourced.STANDARD_ATTACK_TARGET,
+                    Sourced.STANDARD_COLLISION
+                },
+                FollowUpMethod = async a =>
                 {
                     a.AddImplicitResultant(await GameAction.Move.Prompt(a.Performer,
                         new GameAction.Move.PathedInfo(a.ParticipatingUnits[0])
                         {
                             Distance = 1
                         }));
-                }),
-                new Ability.Sourced.TargetingCondition[]
-                {
-                    Ability.Sourced.STANDARD_ATTACK_TARGET,
-                    Ability.Sourced.STANDARD_COLLISION
-                }
-            ),
+                },
 
-            //>1 BREAK WILL
-            () => new Ability.Unsourced
-            (
-                "Break Will", Ability.ETypeIdentity.Utility,
-                new Ability.Unsourced.SingleTargetCondition((p, u) => p.Team != u.Team),
-                new Ability.PlayAction(async a =>
+            },
+
+            () => new Unsourced("Break Will", ETypeIdentity.Utility)
+            {
+                TargetConditions = UnsourcedSingleTarget((p, u) => p.Team != u.Team),
+                ActionMethod = async a =>
                 {
                     a.AddImplicitResultant(await GameAction.Move.Prompt(a.Performer,
                         new GameAction.Move.PathedInfo(a.ParticipatingUnits[0])
                         {
                             Distance = 3
                         }));
-                })
+                }
+            },
 
-
-            ),
-
-            //>2 INSPIRE
-            //uses new format
-            () => new Ability.Sourced
-            (
-                "Inspire", Ability.ETypeIdentity.Defense,
-
-                new ConstructionTemplate<UnitEffect>[]
+            () => new Sourced("Inspire", ETypeIdentity.Defense)
+            {
+                TargetEffects = new()
                 {
                     () => new UnitEffect.Shield(STD_DURATION)
                 },
-                new HashSet<Vector3Int>
+                HitArea = new()
                 {
                     Hit(1, 0, 0),
                     Hit(2, 0, 0),
@@ -89,21 +78,21 @@ public static class AbilityRegistry
                     Hit(1, 1, 0),
                     Hit(0, 1, 1)
                 },
-                new Ability.PlayAction(async a =>
+                TargetingConditions = new()
+                {
+                    Sourced.STANDARD_DEFENSE_TARGET,
+                    Sourced.STANDARD_COLLISION
+                },
+                FollowUpMethod = async a =>
                 {
                     a.AddImplicitResultant(await GameAction.Move.Prompt(a.Performer,
                         new GameAction.Move.PathedInfo(a.ParticipatingUnits[1])
                         {
                             Distance = 5
                         }));
-                }),
-                new Ability.Sourced.TargetingCondition[]
-                {
-                    Ability.Sourced.STANDARD_DEFENSE_TARGET,
-                    Ability.Sourced.STANDARD_COLLISION
                 }
 
-            )
+            }
         };
 
 
@@ -120,6 +109,6 @@ public static class AbilityRegistry
     /// <param name="right"></param>
     /// <returns></returns>
     private static Vector3Int Hit(int left, int up, int right) => BoardCoords.Simple(left, up, right);
-    private static List<TargetCondition> UnsourcedSingleTarget(Func<Player, Unit, bool> singleUnitCondition) =>
+    private static List<Unsourced.TargetCondition> UnsourcedSingleTarget(Func<Player, Unit, bool> singleUnitCondition) =>
             new() { (p, _, u) => singleUnitCondition(p, u) };
 }

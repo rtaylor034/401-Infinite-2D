30f6b13e76ef7988596550a3ab4795a65ae573f7
GFunction <ryan.mm.taylor@gmail.com>
2023-03-22 17:17:47 -0700

    Update CameraSpace.cs

--- Assets/CameraSpace.cs
+++ Assets/CameraSpace.cs
@@ -11,47 +11,37 @@ public class CameraSpace : MonoBehaviour
     private static CameraSpace _instance;
 
     private float _baseSize;
-    private HashSet<GameObject> _linkedObjects;
+    private Dictionary<GameObject, Vector2> _links = new();
 
     private void Awake()
     {
-        SetCamera(Camera.main);
+        SetCamera(Camera.main, true);
         _instance = this;
+        Link(test, 0.2f, 0.8f);
     }
     private void Update()
     {
-        test.transform.position = ActiveCamera.ViewportToWorldPoint(new Vector3(0f, 0.2f, 1));
-        test.transform.localScale = Vector3.one * ActiveCamera.orthographicSize / _baseSize;
-    }
-
-    private void FixedUpdate()
-    {
-        print(ActiveCamera.pixelRect);
-    }
-    private void OnViewportChange()
-    {
-        foreach (var obj in _linkedObjects)
+        foreach (var link in _links)
         {
-
+            link.Key.transform.position = ActiveCamera.ViewportToWorldPoint(new Vector3(link.Value.x, link.Value.y, 1));
+            link.Key.transform.localScale = Vector3.one * ActiveCamera.orthographicSize / _baseSize;
         }
     }
-    private void OnCameraResize()
-    {
 
-    }
-
-    private void SetCamera(Camera camera)
+    private void SetCamera(Camera camera, bool setBaseSize)
     {
         ActiveCamera = camera;
-        _baseSize = Camera.main.orthographicSize;
+        if (setBaseSize) _baseSize = Camera.main.orthographicSize;
         transform.SetParent(camera.transform);
     }
 
     //returns false if obj is already linked. still updates position
-    public static bool Link(GameObject obj)
+    public static bool Link(GameObject obj, float x, float y) => Link(obj, new Vector2(x, y));
+    public static bool Link(GameObject obj, Vector2 pos)
     {
         obj.transform.SetParent(_instance.transform);
-        return _instance._linkedObjects.Add(obj);
-        
+        if (_instance._links.TryAdd(obj, pos)) return true;
+        _instance._links[obj] = pos;
+        return false;
     }
 }

bcb69e54356acd7cdc4c3f3a7f875e84d6f12338
GFunction <ryan.mm.taylor@gmail.com>
2023-03-24 16:08:28 -0700

    GameObject -> Transform anchor

--- Assets/Framework/ScreenSpace.cs
+++ Assets/Framework/ScreenSpace.cs
@@ -10,7 +10,7 @@ public class ScreenSpace : MonoBehaviour
     private static ScreenSpace _instance;
 
     private float _baseSize;
-    private Dictionary<GameObject, Vector2> _links = new();
+    private Dictionary<Transform, Vector2> _links = new();
 
     private void Awake()
     {
@@ -21,14 +21,14 @@ public class ScreenSpace : MonoBehaviour
     {
         foreach (var link in _links)
         {
-            link.Key.transform.position = ActiveCamera.ViewportToWorldPoint(new Vector3(link.Value.x, link.Value.y, 1));
-            link.Key.transform.localScale = Vector3.one * ActiveCamera.orthographicSize / _baseSize;
+            link.Key.position = ActiveCamera.ViewportToWorldPoint(new Vector3(link.Value.x, link.Value.y, 1));
+            link.Key.localScale = Vector3.one * ActiveCamera.orthographicSize / _baseSize;
         }
     }
 
     /// <summary>
     /// Sets the camera that defines the "screen space".<br></br>
-    /// > If <paramref name="setBaseSize"/> is <see langword="true"/>, objects will appear as the size that they appear *right now* when linked.
+    /// > If <paramref name="setBaseSize"/> is <see langword="true"/>, anchorects will appear as the size that they appear *right now* when linked.
     /// </summary>
     /// <param name="camera"></param>
     /// <param name="setBaseSize"></param>
@@ -39,48 +39,48 @@ public class ScreenSpace : MonoBehaviour
         transform.SetParent(camera.transform);
     }
 
-    //returns false if obj is already linked. still updates position
+    //returns false if anchor is already linked. still updates position
 
     /// <inheritdoc cref="Link(GameObject, Vector2)"/>
-    public static bool Link(GameObject obj, float x, float y) => Link(obj, new Vector2(x, y));
+    public static bool Link(Transform anchor, float x, float y) => Link(anchor, new Vector2(x, y));
 
     /// <summary>
-    /// Links <paramref name="obj"/> to screen space at the position <paramref name="pos"/>.<br></br>
+    /// Links <paramref name="anchor"/> to screen space at the position <paramref name="pos"/>.<br></br>
     /// (<paramref name="pos"/> coordinates range from (0, 0) bottom-left to (1, 1) top-right)
     /// <br></br><br></br>
     /// Objects linked to screen space move and scale with the camera's view.<br></br>
-    /// <i>i.e. Linked objects will act as a HUD element, fixed to the player's screen.</i>
+    /// <i>i.e. Linked anchorects will act as a HUD element, fixed to the player's screen.</i>
     /// </summary>
     /// <remarks>
-    /// <i>Camera space will entirely override a linked object's transform.</i>
+    /// <i>Camera space will entirely override a linked anchorect's transform.</i>
     /// </remarks>
-    /// <param name="obj"></param>
+    /// <param name="anchor"></param>
     /// <param name="pos"></param>
     /// <returns></returns>
-    public static bool Link(GameObject obj, Vector2 pos)
+    public static bool Link(Transform anchor, Vector2 pos)
     {
-        if (_instance._links.TryAdd(obj, pos)) return true;
-        _instance._links[obj] = pos;
+        if (_instance._links.TryAdd(anchor, pos)) return true;
+        _instance._links[anchor] = pos;
         return false;
     }
     /// <summary>
-    /// Links <paramref name="obj"/> to screen space at its current position on-screen.<br></br><br></br>
+    /// Links <paramref name="anchor"/> to screen space at its current position on-screen.<br></br><br></br>
     /// <i>(See <see cref="Link(GameObject, Vector2)"/>)</i>
     /// </summary>
-    /// <param name="obj"></param>
+    /// <param name="anchor"></param>
     /// <returns></returns>
-    public static bool LinkAtCurrentPosition(GameObject obj) => Link(obj, ActiveCamera.WorldToViewportPoint(obj.transform.position));
+    public static bool LinkAtCurrentPosition(Transform anchor) => Link(anchor, ActiveCamera.WorldToViewportPoint(anchor.position));
 
     /// <summary>
-    /// Unlinks <paramref name="obj"/> from screen space.
+    /// Unlinks <paramref name="anchor"/> from screen space.
     /// </summary>
     /// <remarks>
-    /// <i><paramref name="obj"/> will keep the transform changes that screen space applied while linked.</i>
+    /// <i><paramref name="anchor"/> will keep the transform changes that screen space applied while linked.</i>
     /// </remarks>
-    /// <param name="obj"></param>
+    /// <param name="anchor"></param>
     /// <returns></returns>
-    public static bool Unlink(GameObject obj)
+    public static bool Unlink(Transform anchor)
     {
-        return _instance._links.Remove(obj);
+        return _instance._links.Remove(anchor);
     }
 }
